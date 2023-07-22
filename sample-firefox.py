5b01106ae9976a4edb1211f869f6cc9d3dff495e
ivanovmp <ivanovmihail99@gmail.com>
2023-07-22 19:43:22 +0300

    Update sample-firefox.py

--- sample-firefox.py
+++ sample-firefox.py
@@ -3,4 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+opts = webdriver.FirefoxOptions()
+opts.add_argument("--headless")
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=opts)
